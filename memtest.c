2d739edc6727173c063bb04ca4f10c6c008acd94
minseonp <minseonp@tamu.edu>
2016-09-23 18:42:11 -0500

    Update memtest.c

--- memtest.c
+++ memtest.c
@@ -4,47 +4,42 @@
 #include <getopt.h>
 #include <iostream>
 #include <cstring> //memset
+
 #include "my_allocator.h"
-#include "ackerman.h"
+
 
 using namespace std;
-/
 unsigned long int num_allocations;
+
 int ackerman(int a, int b) {
+ /*
  This is the implementation of the Ackerman function. The function itself is very
    function is very simple (just two recursive calls). We use it to exercise the
    memory allocator (see "my_alloc" and "my_free"). 
    For this, there are additional calls to "gettimeofday" to measure the elapsed
    time.
- 
- //cout << " ?_? " << endl;
+ */
+ cout << "1 ?_? " << endl;
   void * mem;
-
   //The size "to_alloc" of the region to allocate is computed randomly: 
   int to_alloc =  ((2 << (rand() % 19)) * (rand() % 100)) / 100;
   if  (to_alloc < 4) to_alloc = 4;
-
   int result = 0, i;
- //cout << " ?_? " << endl;
+ cout << "2 ?_? " << endl;
   char c;
-
   mem = my_malloc(to_alloc * sizeof(char));
-
   num_allocations++;
-
   if (mem != NULL) {
- //cout << " ?_? " << endl;
+ cout << "3 ?_? " << endl;
     // generate a random byte to fulfill the allocated block of memory
     c = rand() % 128;
     memset(mem, c, to_alloc * sizeof(char));
-
     if (a == 0)
       result = b + 1;
     else if (b == 0)
       result = ackerman(a - 1, 1);
     else
       result = ackerman(a - 1, ackerman(a, b - 1) );
-
     // check memory value before deleting
     for (i = 0; i < to_alloc; i++) {
         if (((char*)mem)[i] != c) {
@@ -52,31 +47,26 @@ int ackerman(int a, int b) {
             break;
         }
     }
-
-    //  my_free(mem); I do not know proper place for it... we have bus error
+ //cout << " ?_? " << endl;
+    //my_free(mem);
   }
-
   return result;
-}*/
+}
+
 
 
 int main(int argc, char ** argv) {
-	int a,b;
+
 	int c;
+	int a;
 	int b = 128;	 // block size = 2^9
 	int M = b * 12;  // mem size = 512kB = 2^10 * 2^7 // so we have space for 12 items
 	void * A;
 	void * B;
 	void * C;
 	void * D;
-
-	cout << "a value(type 3): ";
-	cin >> a;
-	cout << "b value(type 6): ";
-	cin >> b;
-	ackerman(a, b);
 	
-	/*
+	// their getopt() code, keep intact
 	while ((c = getopt(argc, argv, "bs:")) != -1){
 		switch (c){
 			case 'b':
@@ -86,12 +76,19 @@ int main(int argc, char ** argv) {
 				M = atoi(optarg);
 				break;
 		}
-		//ackerman()?
 	}
-	*/
+
+	cout << "a value: ";
+	cin >> a;
+	cout << "b value: ";
+	cin >> b;
+	init_allocator(1024,64);
+	ackerman(a, b);
+	cout << "bye" << endl;
+	/*
 	cout <<"initialize"<<endl;
 	init_allocator(1024,64);
-	/*printlist();
+	printlist();
 	cout <<"allocate 20"<<endl;
 	A = my_malloc(20);
 
@@ -101,30 +98,31 @@ int main(int argc, char ** argv) {
 	cout <<"allocate 20"<<endl;
 	B = my_malloc(20);
 	C = my_malloc(20);
-	//cout << "A address: " << (A-16) << endl;
-	//cout << "B address: " << (B-16) << endl;
+	cout << "real A   : " << A << endl; 
+	cout << "A address: " << (A-16) << endl;
+	cout << "B address: " << (B-16) << endl;
 	
 
 	//Test function
 	printlist();
 
-	//cout << "A buddy = " << ((void*)(((unsigned long)(A))^(((node*)(A-16))->size))-16)<<endl;
-	//cout << "B buddy = " << ((void*)(((unsigned long)(B))^(((node*)(B-16))->size))-16)<<endl;
+	cout << "A buddy = " << ((void*)(((unsigned long)(A))^(((node*)(A-16))->size))-16)<<endl;
+	cout << "B buddy = " << ((void*)(((unsigned long)(B))^(((node*)(B-16))->size))-16)<<endl;
 
-
-	my_free(A);
-	printlist();
+	my_free(A); 
+	//cout << "A    :" << A << endl;
+	//cout << "A-16 :" << A-16 << endl;
 	my_free(B);
-	printlist();*/
+
 
 	//cout << "B buddy free = " << ((node*)(B-16))->free << endl;
-
 
   // input parameters (basic block size, memory length)
 
   // init_allocator(basic block size, memory length)
 
-  ackerman_main();
-
-  release_allocator();
+  //ackerman_main();
+*/
+  
+  // release_allocator()
 }
